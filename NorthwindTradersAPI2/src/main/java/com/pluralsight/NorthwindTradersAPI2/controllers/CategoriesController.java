92204e06088fd1897f1c772563d19c518c675777
Nauman <naumansqb@gmail.com>
2025-12-09 14:13:39 -0500

    Complete with fetching products and categories
    -List all
    -Fetch By specific ID

--- NorthwindTradersAPI2/src/main/java/com/pluralsight/NorthwindTradersAPI2/controllers/CategoriesController.java
+++ NorthwindTradersAPI2/src/main/java/com/pluralsight/NorthwindTradersAPI2/controllers/CategoriesController.java
@@ -1,5 +1,37 @@
 package com.pluralsight.NorthwindTradersAPI2.controllers;
 
+import com.pluralsight.NorthwindTradersAPI2.models.Category;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class CategoriesController {
-    
+    private List<Category> categories;
+
+    public CategoriesController() {
+        categories = new ArrayList<>();
+        categories.add(new Category(1, "Beverage"));
+        categories.add(new Category(2, "Beverage"));
+        categories.add(new Category(3, "Food"));
+    }
+
+    @RequestMapping(path = "/categories", method = RequestMethod.GET)
+    public List<Category> getAllCategories() {
+        return categories;
+    }
+
+    @RequestMapping(path = "/categories/{categoryId}",method = RequestMethod.GET)
+    public Category getCategoryById(@PathVariable int categoryId){
+        for (Category category : categories) {
+            if(category.getCategoryId() == categoryId){
+                return  category;
+            }
+        }
+        return null;
+    }
 }
--- NorthwindTradersAPI2/src/main/java/com/pluralsight/NorthwindTradersAPI2/controllers/ProductsController.java
+++ NorthwindTradersAPI2/src/main/java/com/pluralsight/NorthwindTradersAPI2/controllers/ProductsController.java
@@ -1,4 +1,34 @@
 package com.pluralsight.NorthwindTradersAPI2.controllers;
 
+import com.pluralsight.NorthwindTradersAPI2.models.Product;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class ProductsController {
+    private List<Product> products;
+
+    public ProductsController(){
+        products=new ArrayList<>();
+        products.add(new Product(1, "john", 1, 18.00));
+        products.add(new Product(2, "Cena", 1, 19.00));
+        products.add(new Product(3, "WWE", 2, 10.00));
+    }
+
+    @RequestMapping(path="/products",method = RequestMethod.GET)
+    public List<Product> getAllProducts (){
+        return products;
+    }
+
+    @RequestMapping(path = "/products/{productId}",method = RequestMethod.GET)
+    public Product getProductById(@PathVariable int productId){
+        for (Product product : products) {
+            if(product.getProductId() == productId){
+                return product;
+            }
+        }
+        return  null;
+    }
 }
